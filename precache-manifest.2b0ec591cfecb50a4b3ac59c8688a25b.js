4c1d4914e4c50f6f10ad314fcdcbbbc9ea6f8769
CI-Leo.Chuang <leo.chuang@cloud-interactive.com>
2019-08-26 03:05:07 +0800

    Updates

---
+++ precache-manifest.2b0ec591cfecb50a4b3ac59c8688a25b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a039d1e693c77b83895dcd558bf16bc",
+    "url": "/hotel-booking/index.html"
+  },
+  {
+    "revision": "221101ae388a6091bbf0",
+    "url": "/hotel-booking/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "f21d79bbb9a961bd7483",
+    "url": "/hotel-booking/static/css/main.9144453e.chunk.css"
+  },
+  {
+    "revision": "221101ae388a6091bbf0",
+    "url": "/hotel-booking/static/js/2.0238f5a1.chunk.js"
+  },
+  {
+    "revision": "f21d79bbb9a961bd7483",
+    "url": "/hotel-booking/static/js/main.a09b2350.chunk.js"
+  },
+  {
+    "revision": "1ad8a6f1d4ed91babdaf",
+    "url": "/hotel-booking/static/js/runtime~main.5e4a2a6d.js"
+  }
+]);
